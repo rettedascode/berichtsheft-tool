a1ae934c0fe48f357e433aa6d50d27746aeffd9d
gharibdilanyan <71387712+gharibdilanyan@users.noreply.github.com>
2024-11-18 11:59:27 +0100

    Templating gebaut, wie haben 1 base layout was für allen gleich bleibt. login bearbeitet. Form für Login erstellt mit validatoren

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, request, send_from_directory, jsonify, render_template
+from flask import Flask, request, send_from_directory, jsonify, render_template, url_for, redirect, flash
 from pathlib import Path
+from flask_wtf import CSRFProtect
+
+from src.forms.LoginForm import LoginForm
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+csrf = CSRFProtect(app)
+csrf._csrf_disable = True
 
 # Directory containing HTML files
 html_dir = Path(__file__).parent
@@ -10,7 +16,8 @@ html_dir = Path(__file__).parent
 # Routes for each HTML file
 @app.route("/azubi-index")
 def get_azubi_index():
-    return send_from_directory(html_dir, "azubi-index.html")
+    return render_template("azubi-index.html")
+    # return send_from_directory(html_dir, "azubi-index.html")
 
 
 @app.route("/")
@@ -20,12 +27,15 @@ def get_index():
 
 @app.route("/login")
 def get_login():
-    return send_from_directory(html_dir, "login.html")
+    form = LoginForm()
+    return render_template('login.html', form=form, errorValues=[])
+    # return send_from_directory(html_dir, "login.html")
 
 
 @app.route("/nachweis-signen")
 def get_nachweis_signen():
-    return send_from_directory(html_dir, "nachweis-signen.html")
+    return render_template("nachweis-signen.html")
+    #return send_from_directory(html_dir, "nachweis-signen.html")
 
 
 # API endpoint for login
@@ -34,10 +44,28 @@ def api_login():
     username = request.form.get("username")
     password = request.form.get("password")
     # Simulate authentication logic (for demonstration purposes)
-    if username == "admin" and password == "password":
-        return jsonify({"message": "Login successful", "redirect": "/azubi-index"})
-    return jsonify({"message": "Invalid credentials"})
+    form = LoginForm()
+    # form.populate_obj({'login': username, 'password': password}.)
+
+    if form.validate():
+        if username == "admin" and password == "password":
+            flash('You were successfully logged in as Admin')
+        else:
+            flash('You were successfully logged in')
+
+        return redirect(url_for('get_azubi_index'))
+
+    else:
+        errorValues = [
+            form.data['login'],
+            form.data['password'],
+        ]
+        flash(
+                'Die Eingabe ist fehlerhaft. Bitte korrigieren.',
+                'danger')
+        return render_template('login.html', form=form, errorValues=errorValues)
+        #return jsonify({"message": "Invalid credentials"})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ src/forms/LoginForm.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField, SubmitField, HiddenField, PasswordField
+from wtforms.validators import DataRequired, Length, Regexp, ValidationError
+
+VERBOTENE_WOERTER = ['beleidigung']
+
+
+def verbotene_woerter_check(form, field):
+    input_value = field.data.lower()
+    for wort in VERBOTENE_WOERTER:
+
+        if wort.lower() in input_value:
+            raise ValidationError(
+                f"Das Wort '{wort}' ist verboten. Bitte verwenden Sie keine beleidigenden Ausdrücke.")
+
+
+class LoginForm(FlaskForm):
+    login = StringField(
+        'Login',
+        validators=[
+            verbotene_woerter_check,
+            DataRequired(),
+            Length(
+                3,
+                40,
+                message="Der Name muss zwischen 3 und 40 Zeichen lang sein.")],
+        render_kw={
+            "placeholder": "Login"},
+        name="login")
+    password = PasswordField(
+        'Password',
+        validators=[
+            verbotene_woerter_check,
+            DataRequired(),
+            Length(
+                3,
+                40,
+                message="Password muss zwischen 3 und 40 Zeichen lang sein.")],
+        render_kw={
+            "placeholder": "*********"},
+        name="password")
+
